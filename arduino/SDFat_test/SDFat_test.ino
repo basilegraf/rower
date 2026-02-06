d9d0591ffee8430758852c1a3e3c7275f4c8d155
Basile <basile.graf@a3.epfl.ch>
2026-02-06 20:53:13 +0100

    Share SPI between SdFat and accuret SPI working

--- arduino/SDFat_test/SDFat_test.ino
+++ arduino/SDFat_test/SDFat_test.ino
@@ -1,7 +1,7 @@
 #include <SPI.h>
 #include "SdFat.h"
 #include "sdios.h"
-
+#include "pins_arduino.h"
 
 #define CHIP_SELECT_SD 10
 #define CHIP_SELECT_ACCURET 14
@@ -11,6 +11,10 @@
 // Change SPI_SPEED to SD_SCK_MHZ(50) for best performance.
 #define SPI_SPEED_SD SD_SCK_MHZ(4)
 
+// AccurEt is slow as slave
+#define SPI_SPEED_ACCURET 500000 
+
+
 SdFat32 sd;
 File32 file;
 
@@ -19,6 +23,7 @@ ArduinoOutStream cout(Serial);
 
 void setup() 
 {
+  // Serial setup
   Serial.begin(9600);
 
   // Wait for USB Serial
@@ -26,6 +31,9 @@ void setup()
   {
     yield();
   }
+
+  // SD card and open file
+  
   cout << F("\nSPI pins:\n");
   cout << F("MISO: ") << int(MISO) << endl;
   cout << F("MOSI: ") << int(MOSI) << endl;
@@ -33,6 +41,7 @@ void setup()
   cout << F("SS:   ") << int(SS) << endl;
   cout << F("SDCARD_SS_PIN:   ") << int(CHIP_SELECT_SD) << endl;
 
+  // Deselect accuret 
   pinMode(CHIP_SELECT_ACCURET, OUTPUT);
   digitalWrite(CHIP_SELECT_ACCURET, HIGH);
 
@@ -47,31 +56,73 @@ void setup()
   file = sd.open("test.txt", FILE_WRITE);
 
   // if the file opened okay, write to it:
-  if (file) {
-    Serial.print("Writing to test.txt...");
-    file.println("testing 1, 2, 3.");
-    // close the file:
- //   file.close();
+  if (file) 
+  {
+    Serial.println("File opened");
   //  Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
+  } else 
+  {
     Serial.println("error opening test.txt");
   }
+
+
+  // SPI setting for accuret communication
+  SPI.begin();
   
 }
 
 
 
-void loop() {
+void loop() 
+{
+  if (file)
+  {
+    Serial.println("Loop begin");
+    file.println("Loop begin");
+  }
 
+
+  byte command, lastCommand, response, expected;
+  lastCommand = 0;
   for (byte k = 0; k < 10; k++)
   {
+    
+    // Send data to accuret
+    digitalWrite(CHIP_SELECT_SD, HIGH);
+    digitalWrite(CHIP_SELECT_ACCURET, LOW);
+    SPI.beginTransaction(SPISettings(SPI_SPEED_ACCURET, MSBFIRST, SPI_MODE0));
+    command = k;
+    response = SPI.transfer(command); // call multiple times of use buffered version
+    SPI.endTransaction();
+    digitalWrite(CHIP_SELECT_ACCURET, HIGH);
+    digitalWrite(CHIP_SELECT_SD, LOW);
+    
+    expected = (lastCommand + byte(77)) & 0xFF;
     if (file)
     {
-      Serial.println(k);
-      file.println(k);
+      Serial.print(lastCommand);
+      Serial.print(" -> ");
+      Serial.print(response);
+      Serial.print(", expected : ");
+      Serial.println(expected);
+
+      file.print(lastCommand, DEC);
+      file.print(" -> ");
+      file.print(response, DEC);
+      file.print(", expected : ");
+      file.println(expected, DEC);
     }
+
+    lastCommand = command;
   }
+
+  if (file)
+  {
+    Serial.println("Loop end");
+    file.println("Loop end");
+  }
+
+  SPI.end();
 
   file.close();
   sd.end();
